0752476bb2a0efcbd32d62d9ab85a8e712da98b2
Christoph Laimer <cla@gmx.ch>
2018-07-29 18:29:02 +0200

    - enhance ggRunningAveragesT

--- LibBase/ggRunningAverages.h
+++ LibBase/ggRunningAverages.h
@@ -1,7 +1,8 @@
 #ifndef GGRUNNINGAVERAGEST_H
 #define GGRUNNINGAVERAGEST_H
 
-#include "ggNumberTypes.h"
+#include "LibBase/ggNumberTypes.h"
+#include "LibBase/ggRound.h"
 
 /**
  * Computes the averages: Sum, Min, Max, Mean, StdDev, and
@@ -16,27 +17,27 @@ class ggRunningAveragesT
 public:
 
   inline ggRunningAveragesT()
-    : mSum(0),
-      mSumSquare(0),
-      mCount(0),
-      mMin(),
-      mMax() {
+  : mSum(0),
+    mSumOfSquares(0),
+    mCount(0),
+    mMin(),
+    mMax() {
   }
 
   // reset the running averages
   inline void Reset() {
     mSum = 0;
-    mSumSquare = 0;
+    mSumOfSquares = 0;
     mCount = 0;
     mMin = TValueType();
     mMax = TValueType();
   }
 
-  // adds a sample for computing an average
+  // adds a sample
   inline void AddSample(TValueType aValue, ggDouble aCount = 1.0) {
     ggDouble vValue = static_cast<ggDouble>(aValue);
     mSum += aCount * vValue;
-    mSumSquare += aCount * vValue * vValue;
+    mSumOfSquares += aCount * vValue * vValue;
     if (mCount == 0) {
       mMin = aValue;
       mMax = aValue;
@@ -50,6 +51,18 @@ public:
       }
     }
     mCount += aCount;
+  }
+
+  // removes a sample (min and max are not updated)
+  inline void RemoveSample(TValueType aValue, ggDouble aCount = 1.0) {
+    ggDouble vValue = static_cast<ggDouble>(aValue);
+    mSum -= aCount * vValue;
+    mSumOfSquares -= aCount * vValue * vValue;
+    mCount -= aCount;
+    if (mCount == 0) {
+      mMin = aValue;
+      mMax = aValue;
+    }
   }
 
   // returns the number of samples
@@ -74,27 +87,63 @@ public:
 
   // returns the mean value
   inline TValueType GetMean() const {
-    if (mCount != 0) {
-      return static_cast<TValueType>(mSum / mCount);
+    if (mCount > 0) {
+      return ggRound<TValueType>(mSum / mCount);
     }
     return TValueType();
   }
 
-  // returns the standart deviation
+  // sum of squared errors, also known as "residual sum of scquares" (RSS, or SSR, or SSE)
+  inline TValueType GetSumOfSquaredErrors() const {
+    if (mCount > 0) {
+      return ggRound<TValueType>(mSumOfSquares - mSum * mSum / mCount);
+    }
+    return TValueType();
+  }
+
+  // variance of sample (square of standard deviation)
+  inline TValueType GetVariance() const {
+    if (mCount > 1) {
+      ggDouble vSumOfSquaredErrors = mSumOfSquares - mSum * mSum / mCount;
+      return ggRound<TValueType>(vSumOfSquaredErrors / (mCount - 1));
+    }
+    return TValueType();
+  }
+
+  // variance of population (square the standard deviation)
+  inline TValueType GetVarianceP() const {
+    if (mCount > 0) {
+      ggDouble vSumOfSquaredErrors = mSumOfSquares - mSum * mSum / mCount;
+      return ggRound<TValueType>(vSumOfSquaredErrors / mCount);
+    }
+    return TValueType();
+  }
+
+  // returns the standard deviation of sample
   inline TValueType GetStdDev() const {
-    if (mCount != 0) {
-      ggDouble vMean = mSum / mCount;
-      return static_cast<TValueType>(sqrt((mSumSquare / mCount) - (vMean * vMean)));
+    if (mCount > 1) {
+      ggDouble vSumOfSquaredErrors = mSumOfSquares - mSum * mSum / mCount;
+      if (vSumOfSquaredErrors >= 0) return ggRound<TValueType>(sqrt(vSumOfSquaredErrors / (mCount - 1)));
+    }
+    return TValueType();
+  }
+
+  // returns the standard deviation of population
+  inline TValueType GetStdDevP() const {
+    if (mCount > 0) {
+      ggDouble vSumOfSquaredErrors = mSumOfSquares - mSum * mSum / mCount;
+      if (vSumOfSquaredErrors >= 0) return ggRound<TValueType>(sqrt(vSumOfSquaredErrors / mCount));
     }
     return TValueType();
   }
 
   // returns the coefficient of variation
-  inline TValueType GetVariation() const {
-    if (mCount != 0) {
+  inline TValueType GetVariationCoefficient() const {
+    if (mCount > 1) {
       ggDouble vMean = mSum / mCount;
       if (vMean != 0) {
-        return static_cast<TValueType>(sqrt((mSumSquare / mCount) - (vMean * vMean)) / vMean);
+        ggDouble vSumOfSquaredErrors = mSumOfSquares - mSum * mSum / mCount;
+        return ggRound<TValueType>(sqrt(vSumOfSquaredErrors / (mCount - 1)) / vMean);
       }
     }
     return TValueType();
@@ -103,7 +152,7 @@ public:
 private:
 
   ggDouble mSum;
-  ggDouble mSumSquare;
+  ggDouble mSumOfSquares;
   ggDouble mCount;
   TValueType mMin;
   TValueType mMax;
