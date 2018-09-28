660b8082f8d68be3b59faa872e833c9397074dbb
Christoph Laimer <cla@gmx.ch>
2018-09-28 20:32:24 +0200

    - fix "In" (index wrong)

--- LibBase/ggFilterT.h
+++ LibBase/ggFilterT.h
@@ -28,21 +28,21 @@ public:
 
   ggFilterMedianT(ggUSize aLength)
   : mLength(aLength),
-    mIndex(0),
+    mIndex(static_cast<ggUSize>(-1)),
     mValues() {
   }
 
   virtual void Reset() override {
     mValues.clear();
-    mIndex = 0;
+    mIndex = static_cast<ggUSize>(-1);
   }
 
   virtual const TValueType& Filter(const TValueType& aInputValue) override {
 
     // store input values in ring-buffer
+    mIndex = (mIndex + 1) % mLength;
     mValues.resize(std::max(mValues.size(), mIndex + 1));
     mValues[mIndex] = aInputValue;
-    mIndex = (mIndex + 1) % mLength;
 
     // copy buffer, partially sort and get median
     std::vector<TValueType> vValuesSorted(mValues);
@@ -85,13 +85,13 @@ public:
 
   ggFilterMeanT(ggUSize aLength)
   : mLength(aLength),
-    mIndex(0),
+    mIndex(static_cast<ggUSize>(-1)),
     mValues() {
   }
 
   virtual void Reset() override {
     mValues.clear();
-    mIndex = 0;
+    mIndex = static_cast<ggUSize>(-1);
   }
 
   virtual const TValueType& Filter(const TValueType& aInputValue) override {
@@ -135,101 +135,5 @@ private:
 typedef ggFilterMeanT<ggInt32> ggFilterMeanInt32;
 typedef ggFilterMeanT<ggFloat> ggFilterMeanFloat;
 typedef ggFilterMeanT<ggDouble> ggFilterMeanDouble;
-
-
-template <class TValueType>
-class ggFilterMedianBesiT : public ggFilterInterfaceT<TValueType> {
-
-public:
-
-  ggFilterMedianBesiT(const TValueType& aLimit)
-  : mLimit(aLimit) {
-    Reset();
-  }
-
-  virtual void Reset() override {
-    mRepeat = false;
-    mX0 = TValueType();
-    mX1 = TValueType();
-    mX2 = TValueType();
-    mXAct = TValueType();
-    mXCorr = TValueType();
-  }
-
-  virtual const TValueType& Filter(const TValueType& aXAct) override {
-
-    mXAct = aXAct;
-
-    if (mRepeat) {
-      mX2 = mX1;
-      mX1 = mX0;
-      mX0 = mXAct;
-      mXCorr = GetMedian(mX0, mX1, mX2);
-      mRepeat = false;
-    }
-    else if (GetAbs(mXAct - mX0) >= mLimit) {
-      mX1 = mX0;
-      mX0 = mXAct;
-      mXCorr = GetMedian(mX0, mX1);
-      mRepeat = true;
-    }
-    else {
-      mX0 = mXAct;
-      mXCorr = mX0;
-    }
-
-    return mXCorr;
-  }
-
-  virtual const TValueType& GetIn() const override {
-    return mXAct;
-  }
-
-  virtual const TValueType& GetOut() const override {
-    return mXCorr;
-  }
-
-private:
-
-  static TValueType GetAbs(const TValueType& aX) {
-    return aX < TValueType() ? -aX : aX;
-  }
-
-  static TValueType GetMedian(const TValueType& aX0,
-                              const TValueType& aX1) {
-    return (aX0 + aX1) / 2;
-  }
-
-  static const TValueType& GetMedian(const TValueType& aX0,
-                                     const TValueType& aX1,
-                                     const TValueType& aX2) {
-    if (aX0 < aX1) {
-      if (aX1 < aX2) return aX1;
-      else if (aX0 < aX2) return aX2;
-      else return aX0;
-    }
-    else {
-      if (aX2 < aX1) return aX1;
-      else if (aX0 < aX2) return aX0;
-      else return aX2;
-    }
-  }
-
-  const TValueType mLimit;
-
-  bool mRepeat;
-
-  TValueType mX0;
-  TValueType mX1;
-  TValueType mX2;
-
-  TValueType mXAct;
-  TValueType mXCorr;
-
-};
-
-typedef ggFilterMedianBesiT<ggInt32> ggFilterMedianBesiInt32;
-typedef ggFilterMedianBesiT<ggFloat> ggFilterMedianBesiFloat;
-typedef ggFilterMedianBesiT<ggDouble> ggFilterMedianBesiDouble;
 
 #endif // GGFILTERT_H
